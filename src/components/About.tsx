e7d94d9b795c348a087a6c24836e53bafe071b18
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2025-08-03 21:37:59 +0200

    typo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 const About: React.FC = () => {
   return (
     <div className="about-section">
-      <h2>O nas</h2>
-      <p>Jesteśmy zespołem programistów, którzy tworzą aplikację do zarządzania książkami.</p>
-      <p>Nasza aplikacja pozwala na:</p>
+      <h2>About Us</h2>
+      <p>We are a team of developers creating a book management application.</p>
+      <p>Our application allows you to:</p>
       <ul>
-<li>Dodawanie nowych książek do kolekcji</li>
-        <li>Przeglądanie listy książek</li>
-        <li>Ocenianie książek</li>
-        <li>Dodawanie opisów i zdjęć</li>
-        <li>Usuwanie książek z kolekcji</li>
+        <li>Add new books to your collection</li>
+        <li>Browse your book list</li>
+        <li>Rate books</li>
+        <li>Add descriptions and photos</li>
+        <li>Remove books from your collection</li>
       </ul>
     </div>
   );
